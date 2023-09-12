b68f74ba0c48d245ce6d5a46df7255323f433718
Felipe <felipeleitectt@gmail.com>
2023-09-12 06:37:43 -0300

    Upload

--- exercicios/ex004/script.js
+++ exercicios/ex004/script.js
@@ -1,1 +1,4 @@
 var nome = window.prompt('Qual seu nome?')
+document.write(`Olá <strong>${nome}</strong>. Seu nome tem ${nome.length} letras.</br>`)
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()}</br>`)
+document.write(`Seu nome em maiúsculas é ${nome.toLowerCase()}`)
