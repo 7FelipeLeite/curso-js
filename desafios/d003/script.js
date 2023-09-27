348a6daa11e001682bfb635fc24880f8977ef8a6
Felipe <134520197+7FelipeLeite@users.noreply.github.com>
2023-09-27 16:12:36 -0300

    Add files via upload

---
+++ desafios/d003/script.js
@@ -0,0 +1,5 @@
+function click1() {
+    let n1 = Math.trunc(Number(window.prompt('Digite um número')))
+    window.alert(`Antes de ${n1} temos o número ${Math.trunc(n1-1)}. Depois de ${n1} temos o número ${Math.trunc(n1+1)}`)
+}
+
